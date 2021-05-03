7c5fb37ee73ad8c099e253bec995b94ea8e6c021
mimura <musyu36@gmail.com>
2021-05-03 15:51:30 +0900

    feature: chordpanelに基本的なスタイル適用

--- components/ChordPanel.tsx
+++ components/ChordPanel.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { Chord } from '../interfaces'
+import styles from '../styles/component/ChordPanel.module.scss'
 
 type Props = {
   chord: Chord
 }
 
 const ChordPanel = ({ chord }: Props) => (
-  <div>
+  <div className={`${styles.panel} shadow`}>
     <h3>
       {chord.root.name}
       {chord.chordType.name}
